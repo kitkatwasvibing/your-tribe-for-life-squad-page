66f18e8bc806dfa00fc315eac95ed45ac0ece7a7
Mete2004 <90602562+Mete2004@users.noreply.github.com>
2026-09-04 19:37:13 +0200

    basic styling van de student-card component #13

--- src/lib/components/studentcard.svelte
+++ src/lib/components/studentcard.svelte
@@ -2,13 +2,53 @@
 	let { person } = $props();
 </script>
 
-<article>
+<article class="student-card">
 	<img
+		class="person-mugshot"
 		src={`https://fdnd.directus.app/assets/${person.mugshot}`}
 		alt={person.name}
+		width="200"
+		height="200"
 	>
 
-	<p>{person.name}</p>
+	<p class="person-name">{person.name}</p>
 
-	<a href={person.profilecard}>Profilecard</a>
+	<a class="profile-link" href={person.profilecard}>Profilecard</a>
 </article>
+
+
+<style>
+	.student-card{
+		background: rgb(238, 238, 238);
+		padding: .7rem;
+		width: 160px;
+		height: 195px;
+		border-radius: 5px;
+
+		@media (min-width:768px){
+			width: 230px;
+			height: 230px;
+		}
+	}
+
+	.person-name{
+		text-align: center;
+	}
+
+	.profile-link{
+		display: block;
+		text-align: center;
+	}
+
+	.person-mugshot{
+		width: 160px;
+		height: 130px;
+		display: block;
+		margin: 0 auto;
+
+		@media (min-width:768px){
+			width: 220px;
+			height: 160px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,22 +21,24 @@
     .person-container{
         display:grid;
         grid-template-columns: repeat(2, 1fr);
-        gap:5px;
+        gap:10px;
         justify-content: center;
+        padding: 0 .5rem;
 
             @media (min-width:550px){
                 grid-template-columns: repeat(3, 1fr);
-                gap:5px;
+                gap:15px;
             }
 
             @media (min-width:768px){
             grid-template-columns: repeat(4, 1fr);
-            gap:5px; 
+            gap:15px; 
+            padding: 0 1rem;
             }
 
         @media (min-width:1024px){
             grid-template-columns: repeat(5,1fr);
-            gap: 5px;
+            gap: 15px;
         }
     }
     .person{
