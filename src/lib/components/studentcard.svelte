90b82aa76fb9db5d38b8b863feb1b756017b1b62
Mete2004 <90602562+Mete2004@users.noreply.github.com>
2026-09-04 03:41:25 +0200

    toon studenten via studentcard component

---
+++ src/lib/components/studentcard.svelte
@@ -0,0 +1,14 @@
+<script>
+	let { person } = $props();
+</script>
+
+<article>
+	<img
+		src={`https://fdnd.directus.app/assets/${person.mugshot}`}
+		alt={person.name}
+	>
+
+	<p>{person.name}</p>
+
+	<a href={person.profilecard}>Profilecard</a>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <script>
-    let {data} = $props();
-    const persons = data.persons;
+	import Studentcard from '$lib/components/studentcard.svelte';
+
+	let { data } = $props();
+	const persons = data.persons;
 </script>
- 
+
 {#each persons as person}
-    <img src="https://fdnd.directus.app/assets/{person.mugshot}" alt={person.name}>
-    {person.name} <br>
-    <a href={person.profilecard}>Profilecard</a>
+	<Studentcard {person} />
 {/each}
