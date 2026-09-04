db580fe9d84818b2b16a79f3a46d9852bddebedb
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-04 12:24:48 +0200

    #4 grid toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
 
 <script>
     let {data} = $props();
     const persons = data.persons;
 </script>
  
+ <div class="person-container">
 {#each persons as person}
-    <img src="https://fdnd.directus.app/assets/{person.mugshot}" alt={person.name}>
-    {person.name} <br>
-    <a href={person.profilecard}>Profilecard</a>
+<div class="person">
+    <img class= "person-img"  src="https://fdnd.directus.app/assets/{person.mugshot}" alt={person.name}>
+    <p class="person-name">{person.name} </p>
+    <a class="profilecard" href={person.profilecard}>Profilecard</a>
+    </div>
 {/each}
+</div>
+
+
+<style>
+    .person-container{
+        display:grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap:5px;
+        justify-content: center;
+
+            @media (min-width:550px){
+                grid-template-columns: repeat(3, 1fr);
+                gap:5px;
+            }
+
+            @media (min-width:768px){
+            grid-template-columns: repeat(4, 1fr);
+            gap:5px; 
+            }
+
+        @media (min-width:1024px){
+            grid-template-columns: repeat(5,1fr);
+            gap: 5px;
+        }
+    }
+    .person{
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+    }
+    .person-img{
+        width:clamp(6.25rem, -7.0833rem + 66.6667vw, 10.625rem);
+        height:auto;
+    }
+</style>
