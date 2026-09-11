a62e641f91caa570d50621fb4c9651a973fb0cb6
Kitkatisvibing <kittykatekawaii@gmail.com>
2026-09-11 11:33:33 +0200

    bottom screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,27 @@
 	const persons = data.persons;
 </script>
  
- <h1 class="h1"> Squad 2</h1>
+<div class="ds-container">
+    <h1 class="h1"> Squad 2</h1>
 
- <div class="person-container">
-{#each persons as person}
-	<Studentcard {person} />
-{/each}
+    <div class="person-container">
+    {#each persons as person}
+        <Studentcard {person} />
+    {/each}
+    </div>
 </div>
-
 
 <style>
     .h1{
         text-align:center;
     }
     .person-container{
-        display:grid;
-        grid-template-columns: repeat(2, 1fr);
+        display:flex;
+        flex-wrap: wrap;
         gap:10px;
         justify-content: center;
-        padding: 0 .5rem;
+        height: 35em;
+        overflow-y: scroll;
 
             @media (min-width:550px){
                 grid-template-columns: repeat(3, 1fr);
@@ -37,7 +39,7 @@
             }
 
         @media (min-width:1024px){
-            grid-template-columns: repeat(6,1fr);
+            grid-template-columns: repeat(5,1fr);
             gap: 15px;
 
         }
