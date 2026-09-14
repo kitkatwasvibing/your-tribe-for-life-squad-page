21ca5004202fe743a83a5c98172bac3c3fd85d59
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-14 15:02:13 +0200

    #22 if else select student

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,78 +1,103 @@
 <script>
-	import Studentcard from '$lib/components/studentcard.svelte';
+    import Studentcard from "$lib/components/studentcard.svelte";
 
-	let { data } = $props();
-	const persons = data.persons;
+    let { data } = $props();
+    // svelte-ignore state_referenced_locally
+    const persons = data.persons;
 
     const person = persons[0];
 
-	const mugshot = person.mugshot
-		? `https://fdnd.directus.app/assets/${person.mugshot}`
-		: '/images/profile-dummy.jpeg';
+    const mugshot = person.mugshot
+        ? `https://fdnd.directus.app/assets/${person.mugshot}`
+        : "/images/profile-dummy.jpeg";
 
-    const age = new Date().getFullYear() - new Date(person.birthdate).getFullYear();
+    const age =
+        new Date().getFullYear() - new Date(person.birthdate).getFullYear();
 </script>
 
 <div class="ds-container bottom">
-   <h1 class="h1"> Squad 2</h1>
+    <h1 class="h1">Squad 2</h1>
 
-<article class="student-detail">
-	<img
-        class="mugshot-detail"
-		src={mugshot}
-		alt={person.name}
-		width="200"
-		height="200"
-	>
+    {#if data.selectedPerson}
+        <article class="student-detail">
+            {#if data.selectedPerson.mugshot}
+                <img
+                    class="mugshot-detail"
+                    src={`https://fdnd.directus.app/assets/${data.selectedPerson.mugshot}`}
+                    alt={data.selectedPerson.name}
+                    width="200"
+                    height="200"
+                />
+            {:else}
+                <img
+                    class="mugshot-detail"
+                    src={"/images/profile-dummy.jpeg"}
+                    alt={data.selectedPerson.name}
+                    width="200"
+                    height="200"
+                />
+            {/if}
+            
+            <div class="student-info">
+                <header>
+                    <h2>{data.selectedPerson.name}</h2>
+                    <p>
+                        {new Date().getFullYear() -
+                            new Date(
+                                data.selectedPerson.birthdate,
+                            ).getFullYear()} jaar
+                    </p>
 
-	<div class="student-info">
-		<header>
-			<h2>{person.name}</h2>
-			<p>{age} jaar</p>
+                    <a
+                        href={`https://github.com/${data.selectedPerson.github_handle}`}
+                    >
+                        GitHub
+                    </a>
+                </header>
 
-            <a href={`https://github.com/${person.github_handle}`}>
-				GitHub
-			</a>
-		</header>
+                <dl>
+                    <div class="info-item">
+                        <dt>Nickname</dt>
+                        <dd>{data.selectedPerson.nickname}</dd>
+                    </div>
 
-    <dl>
-        <div class="info-item">
-            <dt>Nickname</dt>
-            <dd>{person.nickname}</dd>
+                    <div class="info-item">
+                        <dt>Favoriet dier</dt>
+                        <dd>{data.selectedPerson.fav_animal}</dd>
+                    </div>
+
+                    <div class="info-item">
+                        <dt>Hobby</dt>
+                        <dd>{data.selectedPerson.fav_hobby}</dd>
+                    </div>
+
+                    <div class="info-item">
+                        <dt>Favoriete keuken</dt>
+                        <dd>{data.selectedPerson.fav_kitchen}</dd>
+                    </div>
+                </dl>
+
+                <a href={data.selectedPerson.profilecard}>Profile card</a>
+            </div>
+        </article>
+    {:else}
+        <section class="start">
+            <p>Select character</p>
+        </section>
+    {/if}
+    <form method="GET">
+        <div class="person-container">
+            {#each persons as person}
+                <Studentcard {person} />
+            {/each}
         </div>
-
-        <div class="info-item">
-            <dt>Favoriet dier</dt>
-            <dd>{person.fav_animal}</dd>
-        </div>
-
-        <div class="info-item">
-            <dt>Hobby</dt>
-            <dd>{person.fav_hobby}</dd>
-        </div>
-
-        <div class="info-item">
-            <dt>Favoriete keuken</dt>
-            <dd>{person.fav_kitchen}</dd>
-        </div>
-    </dl>
-
-		<a href={person.profilecard}>Profile card</a>
-	</div>
-</article>
-
-    <div class="person-container">
-    {#each persons as person}
-        <Studentcard {person} />
-    {/each}
-    </div>
+    </form>
 </div>
 
 <style>
-
-    .student-detail{
+    .student-detail {
         display: grid;
-	    grid-template-columns: 1fr;
+        grid-template-columns: 1fr;
         gap: 2rem;
         padding: 2rem 3rem;
         max-width: 800px;
@@ -98,15 +123,15 @@
         flex-direction: column;
         gap: 1rem;
         width: 100%;
-	    margin: 0 auto;
+        margin: 0 auto;
 
         @media (min-width: 768px) {
-           margin: 0;
-           width: 500px;
+            margin: 0;
+            width: 500px;
         }
     }
 
-    .student-info h2{
+    .student-info h2 {
         margin: 0;
     }
 
@@ -123,7 +148,7 @@
         margin: 0;
 
         @media (min-width: 768px) {
-           	display: grid;
+            display: grid;
             grid-template-columns: repeat(2, 1fr);
             gap: 0.75rem 4rem;
             margin: 0;
@@ -147,46 +172,34 @@
         text-align: right;
     }
 
-
-
-    .h1{
-        text-align:center;
+    .h1 {
+        text-align: center;
     }
-    .person-container{
-        display:flex;
+    .person-container {
+        display: flex;
         flex-wrap: wrap;
         flex-direction: column;
-        gap:10px;
+        gap: 10px;
         justify-content: center;
         height: 50vh;
         overflow-x: scroll;
         scrollbar-color: darkred lightgray;
         scrollbar-width: thin;
 
-            @media (min-width:550px){
-                grid-template-columns: repeat(3, 1fr);
-                gap:15px;
-            }
-
-            @media (min-width:768px){
-            grid-template-columns: repeat(4, 1fr);
-            gap:15px; 
-            padding: 0 7rem;
-            }
-
-        @media (min-width:1024px){
-            grid-template-columns: repeat(5,1fr);
+        @media (min-width: 550px) {
+            grid-template-columns: repeat(3, 1fr);
             gap: 15px;
-
         }
-    }
-    .person{
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-    }
-    .person-img{
-        width:clamp(6.25rem, -7.0833rem + 66.6667vw, 10.625rem);
-        height:auto;
+
+        @media (min-width: 768px) {
+            grid-template-columns: repeat(4, 1fr);
+            gap: 15px;
+            padding: 0 7rem;
+        }
+
+        @media (min-width: 1024px) {
+            grid-template-columns: repeat(5, 1fr);
+            gap: 15px;
+        }
     }
 </style>
